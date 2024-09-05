b826a25d51d1a2fdaa541f1fddbf8c2d42019438
Lse-Yeong <ocdee39@gmail.com>
2024-09-05 16:36:46 +0900

    error: vercel API 응답 오류 수정

--- kordlefront/src/page/GamePage.js
+++ kordlefront/src/page/GamePage.js
@@ -84,6 +84,8 @@ function GamePage(){
 
     const expirationTime = new Date();
 
+    console.log(todayWord)
+
     if(12<=expirationTime.getHours() && expirationTime.getHours()<24){ //자정 ~ 정오
         expirationTime.setDate(expirationTime.getDate()+1)
     }
